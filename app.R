c63d4e80bdb4007f22404450e1ffa498b3f8a47c
Emilio Santa Cruz <emiliosc4@email.arizona.edu>
2021-10-12 23:57:04 -0700

    Completed price and popularity plots

--- app.R
+++ app.R
@@ -8,9 +8,21 @@ crypto_data <- read.csv("data/All_combined.csv")
 dated_crypto_data <- crypto_data %>%
     mutate(Date = parse_date(`Date`, "%Y-%m-%d"))
 
-crypto_options <- crypto_data %>%
-    distinct(Currency_Name) %>%
-    arrange(Currency_Name)
+price_crypto_options <- crypto_data %>%
+    group_by(Currency_Name) %>%
+    summarize(max_price = max(Price)) %>%
+    arrange(desc(max_price)) %>%
+    distinct(Currency_Name)
+
+popularity_crypto_options <- crypto_data %>%
+    group_by(Currency_Name) %>%
+    summarize(max_vol = max(Vol.)) %>%
+    arrange(desc(max_vol)) %>%
+    distinct(Currency_Name)
+
+
+
+
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -23,6 +35,7 @@ ui <- fluidPage(
         sidebarPanel(
             h3("Price, Popularity and Prediction of top 50 cryptocurrencies"),
             p("Click on tabs to choose difference aspects."),
+            p("Selections for crypto ordered by highest current aspect."),
             HTML("Data derived from <a href=https://www.kaggle.com/odins0n/top-50-cryptocurrency-historical-prices>Kaggle</a>")
         ),
 
@@ -30,19 +43,19 @@ ui <- fluidPage(
         mainPanel(
            tabsetPanel(type = "tabs",
                        tabPanel("Price",
-                                selectInput("Cryptocurrency",
+                                selectInput("price",
                                             "Select Crypto to highlight:",
-                                            choices = crypto_options),
+                                            choices = price_crypto_options),
                                 plotOutput("price_plot")),
                        tabPanel("Popularity",
-                                selectInput("Cryptocurrency",
+                                selectInput("popularity",
                                             "Select Crypto to highlight:",
-                                            choices = crypto_options),
+                                            choices = popularity_crypto_options),
                                 plotOutput("popularity_plot")),
                        tabPanel("Predictions",
-                                selectInput("Cryptocurrency",
+                                selectInput("prediction",
                                             "Select Crypto to highlight:",
-                                            choices = crypto_options),
+                                            choices = price_crypto_options),
                                 plotOutput("prediction_plot")))
         )
     )
@@ -53,10 +66,36 @@ server <- function(input, output) {
 
     output$price_plot <- renderPlot({
         
+        highlighted_region <- dated_crypto_data %>%
+            filter(Currency_Name == input$price)
+        
+        dated_crypto_data %>%
+            ggplot(aes(x = Date,
+                       y = Price,
+                       group = Currency_Name)) + 
+            geom_line(color = "black") + 
+            geom_point(data = highlighted_region,
+                       color = "red") + 
+            geom_line(data = highlighted_region,
+                      color = "red")+
+            theme_linedraw()
+            
     })
     
     output$popularity_plot <- renderPlot({
+        highlighted_region <- dated_crypto_data %>%
+            filter(Currency_Name == input$popularity)
         
+        dated_crypto_data %>%
+            ggplot(aes(x = Date,
+                       y = Vol.,
+                       group = Currency_Name)) + 
+            geom_line(color = "black") + 
+            geom_point(data = highlighted_region,
+                       color = "red") + 
+            geom_line(data = highlighted_region,
+                      color = "red")+
+            theme_linedraw()
     })
     
     output$prediction_plot <- renderPlot({
